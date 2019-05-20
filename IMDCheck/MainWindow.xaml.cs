23bc99d7d7d884df2f0fca760f36a12f0e7ff2ed
texas112 <windows@webloren.com>
2019-05-20 10:45:57 -0500

    First upload

---
+++ IMDCheck/MainWindow.xaml.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IMDCheck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void F1Button_Clicked(object sender, RoutedEventArgs e)
+        {
+            if((bool)f1Button.IsChecked)
+                MessageBox.Show("Test F1 Checked", "Test");
+            else
+                MessageBox.Show("Test F1 Unchecked", "Test");
+        }
+
+        private void F2Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void F3Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void F4Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void F5Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void F6Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void F7Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void F8Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B1Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B2Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B3Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B4Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B5Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B6Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B7Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void B8Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A1Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A2Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A3Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A4Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A5Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A6Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A7Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void A8Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E1Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E2Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E3Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E4Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E5Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E6Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E7Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void E8Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void R1Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void R2Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void R3Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void R4Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void R5Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void R6Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void R7Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void R8Button_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
